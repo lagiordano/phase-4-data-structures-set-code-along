5ad34c8d208f99dde116a7bd4f6aeafbe6979554
Lauren Giordano <laurengiordano94@gmail.com>
2023-07-11 11:16:38 +1000

    done with lab

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,50 @@
 # your code here
+
+class MySet
+    attr_accessor :hash  
+
+    def initialize(enumberable = [])
+        @hash = {}
+        enumberable.each do |value|
+            add(value)
+        end
+    end
+
+
+    def include?(value)
+        hash.has_key?(value)
+    end
+
+    def add(value) 
+        hash[value] = true
+        self
+    end
+
+    def delete(value)
+        hash.delete(value)
+        self
+    end
+
+    def size 
+        hash.size
+    end
+
+    def self.[](*args)
+        new(args)
+    end
+
+    def clear 
+        hash.clear
+        self
+    end
+
+    def each(&block)
+        self.hash.keys.each(&block)
+        self
+    end
+
+    def inspect 
+        "#<#{self.class.name}: {#{self.hash.keys.join(', ')}}>"
+    end
+
+end
